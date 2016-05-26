f7090d562d591b9e1463c832e4a304026e393a01
usrpiro <piotr.rgl@gmail.com>
2016-05-26 11:16:50 +0200

    Bridge Design Pattern - C#

---
+++ Bridge/C#/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Bridge
+{
+    public interface IImageResolution
+    {
+        void DrawResolution(Image img);
+    }
+
+    class A2Image : IImageResolution
+    {
+
+        public void DrawResolution(Image img)
+        {
+            string imgFormat = img.GetImageExtension();
+            Console.WriteLine( imgFormat+" in A2 format has been drawn.");
+        }
+    }
+
+    class A4Image : IImageResolution
+    {
+        public void DrawResolution(Image img)
+        {
+            string imgFormat = img.GetImageExtension();
+            Console.WriteLine(imgFormat + " in A4 format has been drawn.");
+        }
+    }
+
+    public abstract class Image
+    {
+        protected IImageResolution imgResolution;
+        public Image(IImageResolution image)
+        {
+            this.imgResolution = image;
+        }
+
+        public abstract void Draw();
+        public abstract string GetImageExtension();
+    }
+
+    public class JpgImage : Image
+    {
+        public JpgImage(IImageResolution imgResolution) : base(imgResolution) { }
+
+        public override void Draw()
+        {
+            base.imgResolution.DrawResolution(this);
+        }
+
+        public override string GetImageExtension()
+        {
+            return "JPG";
+        }
+    }
+
+    class Bridge
+    {
+        static void Main(string[] args)
+        {
+            Image imgA2 = new JpgImage(new A2Image());
+            Image imgA4 = new JpgImage(new A4Image());
+
+            imgA2.Draw();
+            imgA4.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
